5ed5afd88fd35013e365d6b25a8b964b89db4ec2
lvthinh100 <73948433+lvthinh100@users.noreply.github.com>
2020-11-05 16:42:19 +0700

    thu pull files nay xem

---
+++ morning.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	long long l, r, k;
+	queue<long long> q;
+	cin >> l >> r;
+	for (int i = 1; i <= r, i <= 9; i++)
+	{
+		q.push(i);
+	}
+	while (!q.empty())
+	{
+		k = (q.front() * 10 + (q.front() % 10 + 1));
+		if (k <= r && (q.front() % 10 != 9))
+			q.push(k);
+		if (q.front() >= l)
+		{
+			cout << q.front() << " ";
+			q.pop();
+		}
+		if (q.front() < l)
+			q.pop();
+	}
+	return 0;
+}
